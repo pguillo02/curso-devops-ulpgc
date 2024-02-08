2299555c006ab6b865cca27ce759614b244b8569
Pablo Guilló <112557879+pguillo02@users.noreply.github.com>
2024-02-08 19:45:30 +0000

    Update IBookService.java

--- api/bookreview-api-rest/src/main/java/com/inerza/ulpgc/bookReview/core/persistence/IBookService.java
+++ api/bookreview-api-rest/src/main/java/com/inerza/ulpgc/bookReview/core/persistence/IBookService.java
@@ -7,4 +7,12 @@ import java.util.List;
 public interface IBookService {
 
     List<Book> getBookList(int page, int size, String sortDir, String sort);
+
+    void updateBook(Book post);
+
+    Book createBook(Book post);
+
+    Book getBookById(Long id);
+
+    void deleteBook(Long id);
 }
